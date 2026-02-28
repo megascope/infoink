33eeb4151d2ae44ec0e37737adfa73836f7b2da6
SI <samir@kudesia.org>
2026-02-27 22:21:00 -0600

    added packages

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
+# install package dependencies
+# required for Pillow
+sudo apt install libopenblas0 libjpeg-dev zlib1g-dev libfreetype6-dev
+
 if [ ! -d ./.venv ]; then
     python3 -m venv .venv
 fi
